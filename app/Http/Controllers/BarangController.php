0f1f6f20e63a1e6c9f418053ec64d418083a92ee
= <fariswidhiarta123@gmail.com>
2018-02-24 15:08:29 +0700

    delete laravel.log

--- app/Http/Controllers/BarangController.php
+++ app/Http/Controllers/BarangController.php
@@ -36,7 +36,7 @@ public function __construct()
     }
 
     /**
-     * Show the form for creating a new resource.
+     * Show the form for creating a new resource
      *
      * @return \Illuminate\Http\Response
      */
